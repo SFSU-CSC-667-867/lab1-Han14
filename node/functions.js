3c702c89956c03966e77d357bc466198dc87ebf3
Han14 <33738931+Han14@users.noreply.github.com>
2019-02-06 20:28:12 -0800

    Add files via upload

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,14 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y+1;
+//{ return y+1; }
+console.log(myArrow(s));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log("this is wierd");
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+functionFactory = 7;
+anotherFunction()();
